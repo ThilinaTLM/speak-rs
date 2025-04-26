5d2d0f9d131fde4e79bb701569b0f5890b2403ac
Thilina Lakshan <thilinalakshanmail@gmail.com>
2025-04-26 17:01:29 +0530

    Refactor audio transcription logic and UI interaction
    - Removed unused `BackendSelector` import from `slint`.
    - Introduced a new `transcription_timer` to handle periodic transcription updates.
    - Updated the UI to manage transcription display more effectively during recording.
    - Enhanced the logic for starting and stopping timers based on recording state, improving overall user experience.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{sync::Arc, time::Duration};
 use anyhow::Result;
 use arboard::Clipboard;
 use log::LevelFilter;
-use slint::{BackendSelector, PhysicalPosition, Timer, TimerMode};
+use slint::{PhysicalPosition, Timer, TimerMode};
 
 mod capture;
 mod whisper;
@@ -16,10 +16,11 @@ fn main() -> Result<()> {
         .filter_module("whisper_rs", LevelFilter::Off)
         .init();
 
+    let ui = Arc::new(MainWindow::new()?);
     let recorder = Arc::new(capture::SimpleAudioCapture::new());
     let transcriber = Arc::new(whisper::SimpleTranscriber::new());
-    let ui = Arc::new(MainWindow::new()?);
     let duration_timer = Arc::new(Timer::default());
+    let transcription_timer = Arc::new(Timer::default());
 
     let _ui = ui.clone();
     let _recorder = recorder.clone();
@@ -35,26 +36,56 @@ fn main() -> Result<()> {
         }
     });
 
+    let _ui = ui.clone();
     let _recorder = recorder.clone();
-    let _timer = duration_timer.clone();
+    let _transcriber = transcriber.clone();
+    let transcription_timer_fn = Arc::new(move || {
+        let recording = _recorder.get_is_recording();
+        if recording {
+            let audio_data = _recorder.get_audio_data().unwrap_or_default();
+            let sample_rate = _recorder.get_sample_rate().unwrap_or(44100);
+            let channels = _recorder.get_channels().unwrap_or(1);
+            let audio_duration = _recorder.get_duration().unwrap_or(0.0);
+
+            if audio_duration < 2.0 {
+                return;
+            }
+
+            let transcription = _transcriber
+                .transcribe(&whisper::InputAudio {
+                    data: &audio_data,
+                    sample_rate,
+                    channels,
+                })
+                .unwrap();
+
+            _ui.set_transcription(transcription.combined.trim().to_string().into());
+        }
+    });
+
+    let _recorder = recorder.clone();
+    let _duration_timer = duration_timer.clone();
+    let _transcription_timer = transcription_timer.clone();
     ui.on_close_button_clicked(move || {
         _recorder.stop();
-        _timer.stop();
+        _duration_timer.stop();
+        _transcription_timer.stop();
         std::process::exit(0);
     });
 
     let _recorder = recorder.clone();
     let _transcriber = transcriber.clone();
     let _ui = ui.clone();
-    let _timer = duration_timer.clone();
+    let _duration_timer = duration_timer.clone();
+    let _transcription_timer = transcription_timer.clone();
     ui.on_record_button_clicked(move || {
         let recording = _ui.get_recording();
         if recording {
             _recorder.pause();
             _ui.set_recording(false);
-            _timer.stop();
+            _duration_timer.stop();
+            _transcription_timer.stop();
 
-            // transcribing audio
             _ui.set_transcription("transcribing...".into());
             let audio_data = _recorder.get_audio_data().unwrap_or_default();
             let sample_rate = _recorder.get_sample_rate().unwrap_or(44100);
@@ -75,16 +106,24 @@ fn main() -> Result<()> {
 
             _ui.set_transcription(transcription.combined.trim().to_string().into());
         } else {
-            let _timer_fn = duration_timer_fn.clone();
             _ui.set_transcription("".into());
             _recorder.stop();
             _recorder.start();
 
             _ui.set_transcription("".into());
             _ui.set_recording(true);
-            _timer.start(TimerMode::Repeated, Duration::from_millis(500), move || {
-                _timer_fn();
+            let _duration_timer_fn = duration_timer_fn.clone();
+            _duration_timer.start(TimerMode::Repeated, Duration::from_millis(500), move || {
+                _duration_timer_fn();
             });
+            let _transcription_timer_fn = transcription_timer_fn.clone();
+            _transcription_timer.start(
+                TimerMode::Repeated,
+                Duration::from_millis(3000),
+                move || {
+                    _transcription_timer_fn();
+                },
+            );
         }
     });
 
